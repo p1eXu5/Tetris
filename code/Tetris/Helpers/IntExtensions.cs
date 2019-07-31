49bbce130b6e04b63cda9565cd65149b2e79e7a1
First Second <v1adp1exu5@gmail.com>
2019-07-31 16:04:23 +0300

    added IntExtensions::ForEach

---
+++ code/Tetris/Helpers/IntExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Helpers
+{
+    public static class IntExtensions
+    {
+        public static void ForEach( this int n, Action action )
+        {
+            for ( int i = 0; i < n; i++ ) {
+                action();
+            }
+        }
+    }
+}
