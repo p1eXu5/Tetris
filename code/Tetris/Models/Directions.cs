c24721b6c642d2be9a34cec0ef8dfb5613870ce9
First Second <v1adp1exu5@gmail.com>
2019-08-06 09:59:11 +0300

    added Direstions

---
+++ code/Tetris/Models/Directions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tetris.Models
+{
+    public static class Directions
+    {
+        public static Vector DownVector => new Vector( 0.0, 1.0 );
+        public static Vector LeftVector => new Vector( -1.0, 0.0 );
+        public static Vector RightVector => new Vector( 1.0, 0.0 );
+        public static Vector UpVector => new Vector( 0.0, -1.0 );
+    }
+}
